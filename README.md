0e05fdbd55311457eda5702dcf78c2331bdad3f2
Mosa De La Rosa <232691199+mosadlr@users.noreply.github.com>
2026-03-12 14:21:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WMD-Assignment-2026
+WMD Assignment 2026 - Personal Website Development
---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// Navigation Overlay - Side View (rtl)
+const navOverlay = document.getElementById("navOverlay");
+
+function openNav() {
+  navOverlay.style.height = "100%";
+}
+
+function closeNav() {
+  navOverlay.style.height = "0%";
+}
